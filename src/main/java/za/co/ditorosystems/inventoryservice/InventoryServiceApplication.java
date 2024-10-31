58efb34693c81bab3ab6035e2c90fe15557ed39e
dr37 <naledi.madlopha@gmail.com>
2024-10-31 19:41:17 +0200

    Implement datasource setup

--- src/main/java/za/co/ditorosystems/inventoryservice/InventoryServiceApplication.java
+++ src/main/java/za/co/ditorosystems/inventoryservice/InventoryServiceApplication.java
@@ -8,8 +8,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class InventoryServiceApplication {
 
-
-
     public static void main(String[] args) {
         SpringApplication.run(InventoryServiceApplication.class, args);
     }
---
+++ src/main/java/za/co/ditorosystems/inventoryservice/model/Product.java
@@ -0,0 +1,29 @@
+package za.co.ditorosystems.inventoryservice.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+
+@Data
+@Builder
+@Entity
+@Table(name = "product")
+@NoArgsConstructor
+@AllArgsConstructor
+public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String code;
+    private String name;
+    private String description;
+    private BigDecimal price;
+}
